8ebde6d74ca9543ca6dc622f1aab4e3d99ff8936
Andres Mazo <amazobo@eafit.edu.co>
2023-05-18 12:33:36 -0500

    Creando funciones

--- index.js
+++ index.js
@@ -2,3 +2,13 @@ var boton_encriptar = document.querySelector(".encriptar");
 var boton_desencriptar = document.querySelector(".desencriptar");
 var entrada_texto = document.querySelector(".entrada_texto");
 var salida_texto = document.querySelector(".salida_texto");
+
+boton_encriptar.onclick = encriptar;
+
+function encriptar(){
+    ocultarimagen();
+}
+
+function ocultarimagen(){
+    salida_texto.classList.add(".ocultar");
+}
