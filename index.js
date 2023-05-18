4b77e46f2023d47992f65529d40e20963d45b048
Andres Mazo <amazobo@eafit.edu.co>
2023-05-18 12:24:37 -0500

    Creando variables

---
+++ index.js
@@ -0,0 +1,4 @@
+var boton_encriptar = document.querySelector(".encriptar");
+var boton_desencriptar = document.querySelector(".desencriptar");
+var entrada_texto = document.querySelector(".entrada_texto");
+var salida_texto = document.querySelector(".salida_texto");
